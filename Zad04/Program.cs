33cfe0462cdb037ac3e4b16ea5ea58f5fa1473e7
Vladislav <vnl.i@ya.ru>
2022-12-02 23:24:17 +0300

    решение дом задачи 04

---
+++ Zad04/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да 
+// 1 -> нет
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int number = Prompt("Введите число");
+     
+if (number < 8 && number >5) 
+{
+       System.Console.WriteLine($"Да");
+}
+else 
+{
+        System.Console.WriteLine("Нет");
+}
