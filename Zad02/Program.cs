472f6fadea4a264d65cee2f2bb68d4672e5216f1
Vladislav <vnl.i@ya.ru>
2022-12-02 22:32:48 +0300

    решение дом задачи 02

--- Zad02/Program.cs
+++ Zad02/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("25Hello, World!");
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+// и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+Random rnd = new Random();
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+// int number = Prompt("Введите число");
+int number = rnd.Next()%1000;
+                                          //Отрезается одна цифра сзади и одна спереди
+if (number >= 100 && number < 1000)    //  Проверить трехзначность
+{
+    int result = number / 100;
+    int result2 = number / 10 % 10;
+    System.Console.WriteLine($"Вторая цифра случайного числа удалена! {result} {result2}");
+}
+else 
+{
+    System.Console.WriteLine("Число не трехзначное");
+}
