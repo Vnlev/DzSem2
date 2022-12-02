994e142fb607e923e64c9b69a3fde6c149614eec
Vladislav <vnl.i@ya.ru>
2022-12-02 21:55:29 +0300

    решение дом задачи 1

--- Zad01/Program.cs
+++ Zad01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("12Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");   
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;           
+}
+
+int number = Prompt("Введите число");
+int result = number/10 % 10;                  //Отрезается одна цифра и одна спереди
+System.Console.WriteLine(result);
